ab3af2f64b87e845d4606160d48334c413e7ac38
Ian Harrigan <ianharrigan@hotmail.com>
2017-04-29 17:07:47 +0200

    use applyStyle

--- haxe/ui/backend/TextDisplayBase.hx
+++ haxe/ui/backend/TextDisplayBase.hx
@@ -1,18 +1,15 @@
 package haxe.ui.backend;
 
 import haxe.ui.backend.pixi.HtmlUtils;
+import haxe.ui.styles.Style;
 import pixi.core.text.Text;
 import pixi.core.text.TextStyle;
 
 class TextDisplayBase extends Text {
-    private var _style:TextStyle = new TextStyle();
 
     public function new() {
         super("");
         scale = new pixi.core.math.Point(1, 1);
-        this.style = _style;
-        this.fontName = "Arial";
-        this.fontSize = 13;
     }
 
     public var textWidth(get, null):Float;
@@ -43,50 +40,15 @@ class TextDisplayBase extends Text {
         return value;
     }
 
-    public var color(get, set):Int;
-    private function get_color():Int {
-        return _style.fill;
-    }
-    private function set_color(value:Int):Int {
-        _style.fill = HtmlUtils.color(value);
-        this.style = _style;
-
-        return value;
-    }
-
-    private var _fontName:String = "Arial";
-    public var fontName(get, set):String;
-    private function get_fontName():String {
-        return _fontName;
-    }
-    private function set_fontName(value:String):String {
-        _fontName = value;
-        updateFont();
-        return value;
-    }
-
-    private var _fontSize:Float = 13;
-    public var fontSize(get, set):Null<Float>;
-    private function get_fontSize():Null<Float> {
-        return _fontSize;
-    }
-    private function set_fontSize(value:Null<Float>):Null<Float> {
-        _fontSize = value;
-        updateFont();
-        return value;
-    }
-
-    public var textAlign(get, set):Null<String>;
-    private function get_textAlign():Null<String> {
-        return null;
-    }
-    private function set_textAlign(value:Null<String>):Null<String> {
-        return value;
-    }
-
-    private function updateFont() {
-        _style.fontFamily = _fontName;
-        _style.fontSize = _fontSize;
-        this.style = _style;
+    public function applyStyle(style:Style) {
+        if (style.color != null) {
+            this.style.fill = HtmlUtils.color(style.color);
+        }
+        if (style.fontName != null) {
+            this.style.fontFamily = style.fontName;
+        }
+        if (style.fontSize != null) {
+            this.style.fontSize = Std.parseFloat(style.fontSize);
+        }
     }
 }
