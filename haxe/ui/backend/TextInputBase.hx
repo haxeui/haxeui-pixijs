f3742e7e028efd19025fef4beadd502a0b17d039
Ian Harrigan <ianharrigan@hotmail.com>
2018-01-11 19:04:57 +0100

    text input changes

--- haxe/ui/backend/TextInputBase.hx
+++ haxe/ui/backend/TextInputBase.hx
@@ -1,11 +1,21 @@
 package haxe.ui.backend;
 
+import haxe.ui.core.TextInput.TextInputData;
+
 class TextInputBase extends TextDisplayBase {
+    private var _inputData:TextInputData = new TextInputData();
+    
     public function new() {
         super();
     }
-
-    private var _password:Bool = false;
-    private var _hscrollPos:Float = 0;
-    private var _vscrollPos:Float = 0;
+    
+    private override function measureText() {
+        super.measureText();
+        
+        _inputData.hscrollMax = _textWidth - _width;
+        _inputData.hscrollPageSize = (_width * _inputData.hscrollMax) / _textWidth;
+        
+        _inputData.vscrollMax = _textHeight - _height;
+        _inputData.vscrollPageSize = (_height * _inputData.vscrollMax) / _textHeight;
+    }
 }
