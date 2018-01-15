edad65ebf5dd60c3dc3ab86c0d7e77931f82e57a
Valentin Lemière <valentin@ibilon.com>
2018-01-15 08:23:38 +0100

    Toolkit options are optionnal

--- haxe/ui/backend/ToolkitOptions.hx
+++ haxe/ui/backend/ToolkitOptions.hx
@@ -4,6 +4,6 @@ import pixi.core.display.Container;
 import pixi.core.renderers.SystemRenderer;
 
 typedef ToolkitOptions = {
-	stage:Container,
-	renderer:SystemRenderer
+	?stage:Container,
+	?renderer:SystemRenderer
 }
