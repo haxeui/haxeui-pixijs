4725631dd168f1ad11c8b36030ecc5e435fca02e
Ian Harrigan <ianharrigan@hotmail.com>
2018-05-10 19:38:10 +0200

    fix for https://github.com/haxeui/haxeui-core/issues/223

--- haxe/ui/backend/TextDisplayBase.hx
+++ haxe/ui/backend/TextDisplayBase.hx
@@ -56,7 +56,7 @@ class TextDisplayBase {
         if (_fontInfo != null && _fontInfo.data != textField.style.fontFamily) {
             textField.style.fontFamily = _fontInfo.data;
             measureTextRequired = true;
-            parentComponent.invalidateLayout();
+            parentComponent.invalidateComponentLayout();
         }
 
         if (_textStyle.fontSize != null) {
